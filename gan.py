efbdcdba24212296f93d75ee77bf0bc242fd3b2f
Gajan Nagaraj <thegajannagaraj@gmail.com>
2020-03-08 15:45:02 -0700

    updated names

--- gan.py
+++ gan.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import time
 
 
-def CNN_2RNN(X_train_valid, y_train_valid, Xval, yval):
+def CNN2_RNN2(X_train_valid, y_train_valid, Xval, yval):
     model = models.Sequential()
 
     model.add(layers.Permute((2, 1), input_shape=(22, 1000)))
@@ -32,7 +32,7 @@ def CNN_2RNN(X_train_valid, y_train_valid, Xval, yval):
 
     model.add(layers.LSTM(20, return_sequences=True, stateful=False))
     model.add(layers.BatchNormalization())
-    model.add(layers.Dropout(0.5))
+    model.add(layers.Dropout(0.8))
 
     model.add(layers.Flatten())
     model.add(layers.Dense(4, activation='softmax'))
@@ -93,22 +93,28 @@ def RNN2(X_train_valid, y_train_valid, Xval, yval):
 
 def CNN2_FC(X_train_valid, y_train_valid, Xval, yval):
     model = models.Sequential()
+
     model.add(layers.Reshape((22, 1000)))
     model.add(layers.Permute((2, 1), input_shape=(22, 1000)))
+
     model.add(layers.Conv1D(40, kernel_size=20, strides=4))
     model.add(layers.BatchNormalization())
+    model.add(layers.Dropout(0.5))
     model.add(layers.Activation('relu'))
     model.add(layers.Conv1D(40, kernel_size=20, strides=4))
     model.add(layers.BatchNormalization())
+    model.add(layers.Dropout(0.5))
     model.add(layers.Activation('relu'))
     model.add(layers.MaxPooling1D(pool_size=3))
     model.add(layers.Dropout(0.5))
 
     model.add(layers.Conv1D(40, kernel_size=10, strides=4))
     model.add(layers.BatchNormalization())
+    model.add(layers.Dropout(0.5))
     model.add(layers.Activation('relu'))
     model.add(layers.Conv1D(40, kernel_size=3, strides=4))
     model.add(layers.BatchNormalization())
+    model.add(layers.Dropout(0.5))
     model.add(layers.Activation('relu'))
     model.add(layers.MaxPooling1D(pool_size=1))
     model.add(layers.Dropout(0.5))
@@ -116,13 +122,14 @@ def CNN2_FC(X_train_valid, y_train_valid, Xval, yval):
     model.add(layers.Flatten())
     model.add(layers.Dense(256))
     model.add(layers.BatchNormalization())
+    model.add(layers.Dropout(0.5))
     model.add(layers.Activation('relu'))
     model.add(layers.Dropout(0.5))
     model.add(layers.Dense(4))
     model.add(layers.Activation('softmax'))
 
     model.compile('adam', 'sparse_categorical_crossentropy', metrics=['acc'])
-    loss_hist = model.fit(Xtrain, ytrain, validation_data=(Xval, yval), epochs=50)
+    loss_hist = model.fit(Xtrain, ytrain, validation_data=(Xval, yval), epochs=250)
     model.summary()
     hist = loss_hist.history
     plt.figure(figsize=(15, 7))
@@ -145,5 +152,5 @@ def CNN2_FC(X_train_valid, y_train_valid, Xval, yval):
 if __name__ == '__main__':
     Xtrain, ytrain, Xval, yval = train_val_test()
     # RNN2(Xtrain, ytrain, Xval, yval)
-    CNN_2RNN(Xtrain, ytrain, Xval, yval)
-    # CNN(Xtrain, ytrain, Xval, yval)
+    CNN2_RNN2(Xtrain, ytrain, Xval, yval)
+    # CNN2_FC(Xtrain, ytrain, Xval, yval)
